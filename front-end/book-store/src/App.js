a9b2f2f66f888087f31bab96dd4979f311c202e8
LorenzoLim <renzo-lim@hotmail.com>
2018-09-08 23:35:18 +1000

    Book List component created and added to app.js

--- front-end/book-store/src/App.js
+++ front-end/book-store/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
+import BookList from './components/BookList'
+
 //apollo client setup
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -12,6 +14,7 @@ class App extends Component {
     return (
       <ApolloProvider client={client} >
         <div className="App">
+          <Booklist />
         </div>
       </ApolloProvider>
     );
---
+++ front-end/book-store/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+  return(){
+    <div>
+      <ul id="book-list">
+        <li>Book Name</li>
+      </ul>
+    </div>
+  }
+}
+
+export default BookList
