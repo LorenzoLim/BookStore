f4737365e91ebe8b2ed2c11b5a1c0c1ccb8723f2
LorenzoLim <renzo-lim@hotmail.com>
2018-10-27 15:56:55 +1000

    Resolved conflicts

--- back-end/schema/schema.js
+++ back-end/schema/schema.js
@@ -1,5 +1,4 @@
 const graphql = require('graphql')
-<<<<<<< HEAD
 const _ = require('lodash')
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql
@@ -14,21 +13,11 @@ var authors = [
   {name: 'Patrick Rothfuss', age: 44, id:'1'},
   {name: 'Brandon Sanderson', age: 42, id:'2'},
   {name: 'Terry Pratchett', age: 66, id:'3'}
-=======
-const_ = require('lodash')
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
-
-var books = [
-  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
-  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
->>>>>>> e71745627b6d9e9c2a9544ba2d09292593d44f00
 ]
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-<<<<<<< HEAD
     id: {type: GraphQLID},
     name: {type:GraphQLString},
     genre:{type:GraphQLString}
@@ -41,11 +30,6 @@ const AuthorType = new GraphQLObjectType({
     id: {type: GraphQLID},
     name: {type:GraphQLString},
     age:{type:GraphQLInt}
-=======
-    id: {type: GraphQLString},
-    name: {type: GraphQLString},
-    genre: {type: GraphQLString}
->>>>>>> e71745627b6d9e9c2a9544ba2d09292593d44f00
   })
 })
 
@@ -54,7 +38,6 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-<<<<<<< HEAD
       args: {id:{type: GraphQLID}},
       resolve(parent, args){
         return _.find(books,{id: args.id})
@@ -65,11 +48,6 @@ const RootQuery = new GraphQLObjectType({
       args: {id:{type:GraphQLID}},
       resolve(parent,args){
         return _.find(authors, {id:args.id})
-=======
-      args: {id: {type: GraphQLString}},
-      resolve(parents, args) {
-        return _.find(books, {id: args.id})
->>>>>>> e71745627b6d9e9c2a9544ba2d09292593d44f00
       }
     }
   }
