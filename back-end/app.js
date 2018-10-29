dab3c42edf330e577753f2b7b441dea93f1daa69
LorenzoLim <renzo-lim@hotmail.com>
2018-10-29 23:40:28 +1000

    Connected back end to mLab

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
-const mongoose = require('mongoose')
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://johndoe:test123@ds143953.mlab.com:43953/bookstore");
+mongoose.connection.once("open", () => {
+  console.log("Connected to Database");
+});
 
 const app = express();
-app.use('/graphql',graphqlHTTP({
-  schema,
-  graphiql: true
-}))
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
-  console.log('Listening on port 4000');
-})
+  console.log("Listening on port 4000");
+});
